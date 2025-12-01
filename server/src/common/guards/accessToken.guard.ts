85c8519b9092b84604ef94dda7512daa95e79fe6
Tugduoff <tugdual.de-reviers@epitech.eu>
2025-12-01 18:39:32 +0100

    fix: use verifyAccessToken auth service function inside the guard for better maintanability and reponsibilities concessions

--- server/src/common/guards/accessToken.guard.ts
+++ server/src/common/guards/accessToken.guard.ts
@@ -3,19 +3,13 @@ import {
   ExecutionContext,
   Injectable,
   UnauthorizedException,
-  Logger,
-  InternalServerErrorException,
 } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
 
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-
-import { user } from "@entities/user.entity";
+import { AuthService } from "../../modules/auth/auth.service";
 
 /**
- * Guard that verifies the presence and validity of an access token in the Authorization header.
- * If valid, it attaches the userId and user payload to the request object.
+ * Guard that verifies the presence and validity of an access token in cookies.
+ * If valid, it attaches the userId to the request object.
  *
  * Throws UnauthorizedException if the token is missing, malformed, or invalid.
  *
@@ -37,12 +31,7 @@ import { user } from "@entities/user.entity";
  */
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
-  private readonly logger: Logger = new Logger(AccessTokenGuard.name);
-
-  constructor(
-    private readonly jwtService: JwtService,
-    @InjectRepository(user) private readonly userRepository: Repository<user>,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
@@ -55,38 +44,10 @@ export class AccessTokenGuard implements CanActivate {
       );
     }
 
-    // Verify accesstoken JWT
-    let payload;
-    try {
-      payload = await this.jwtService.verifyAsync(token);
-    } catch {
-      throw new UnauthorizedException("Invalid or expired access token");
-    }
+    const user = await this.authService.verifyAccessToken(token);
 
-    // Extract userId from JWT payload
-    const userId = payload.userId;
-    if (!userId)
-      throw new UnauthorizedException("Token payload missing user id");
+    req.userId = user.user_id;
 
-    // Verify user exists in DB
-    let foundUser: any;
-    try {
-      foundUser = await this.userRepository.findOne({
-        where: { user_id: String(userId) },
-      });
-    } catch (err) {
-      this.logger.error(`Failed to find user in repository : ${err.message}`);
-      throw new InternalServerErrorException(
-        "Internal server error while checking existing user.",
-      );
-    }
-
-    if (!foundUser) {
-      throw new UnauthorizedException("User not found");
-    }
-
-    // Success ! User found and has good JWT
-    req.userId = userId;
     return true;
   }
 }
