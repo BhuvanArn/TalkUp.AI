97cddf546ce73ce678857848ab51b4e28048d790
Tugduoff <tugdual.de-reviers@epitech.eu>
2025-12-02 17:49:50 +0100

    feat(switch-button): refactor SwitchButton component to support activeView prop and move types to a separate file

--- web/src/components/atoms/switch-button/index.tsx
+++ web/src/components/atoms/switch-button/index.tsx
@@ -1,40 +1,54 @@
 import { useState } from 'react';
+import { SwitchButtonProps } from './types';
 
-export interface SwitchButtonProps {
-  leftLabel: string;
-  rightLabel: string;
-  onSwitch: (view: 'left' | 'right' ) => void;
-  defaultActive?: 'left' | 'right';
-}
-
+/**
+ * Switch Button component
+ *
+ * @param param0 Props for the SwitchButton component.
+ * @returns the atomic component Switch Button
+ */
 export const SwitchButton = ({
-  leftLabel, rightLabel, onSwitch, defaultActive = 'left'
+  leftLabel,
+  rightLabel,
+  onSwitch,
+  defaultActive = 'left',
+  activeView,
 }: SwitchButtonProps) => {
-  const [active, setActive] = useState<'left' | 'right'>(defaultActive);
+  const [internalActive, setInternalActive] = useState<'left' | 'right'>(
+    defaultActive,
+  );
+
+  const active = activeView ?? internalActive;
 
   const handleSwitch = (view: 'left' | 'right') => {
-    setActive(view);
+    if (activeView === undefined) {
+      setInternalActive(view);
+    }
     onSwitch(view);
   };
 
   return (
-    <div className="flex items-center bg-surface border border-surface-raised rounded-[20px] transition-colors h-8">
+    <div className="flex items-center bg-surface border border-surface-raised rounded-[20px] transition-colors h-6">
       <button
         onClick={() => handleSwitch('left')}
-        className={`px-3 py-0.5 h-8 text-button-s cursor-pointer rounded-[20px] transition-colors ${
-          active === 'left' ? 'text-active bg-surface-raised hover:bg-surface-raised-hover' : 'text-idle'
+        className={`px-3 py-0.5 h-6 text-button-s cursor-pointer rounded-[20px] transition-colors ${
+          active === 'left'
+            ? 'text-active bg-surface-raised hover:bg-surface-raised-hover'
+            : 'text-idle'
         }`}
       >
         {leftLabel}
       </button>
       <button
         onClick={() => handleSwitch('right')}
-        className={`px-3 py-0.5 h-8 text-button-s cursor-pointer rounded-[20px] transition-colors ${
-          active === 'right' ? 'text-active bg-surface-raised hover:bg-surface-raised-hover' : 'text-idle'
+        className={`px-3 py-0.5 h-6 text-button-s cursor-pointer rounded-[20px] transition-colors ${
+          active === 'right'
+            ? 'text-active bg-surface-raised hover:bg-surface-raised-hover'
+            : 'text-idle'
         }`}
       >
         {rightLabel}
       </button>
     </div>
   );
-}
+};
