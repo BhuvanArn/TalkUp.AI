815e6057e3c3b3c483f91e244bdc11654e6391a0
badarouzia <ziane.badarou@epitech.eu>
2026-01-17 06:00:28 +0100

    Ran Prettier to resolve style issues in forgot-password-page/index.tsx and forgot-password.tsx.

--- web/src/routes/forgot-password.tsx
+++ web/src/routes/forgot-password.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router';
 import ForgotPasswordPage from '@/components/organisms/forgot-password-page';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/forgot-password')({
   component: ForgotPasswordPage,
