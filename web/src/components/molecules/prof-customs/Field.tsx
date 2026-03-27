f65041f0da1237c2eb70abfe3406a1ab679adc26
badarouzia <badaroucedene@icloud.com>
2026-03-27 13:31:32 +0100

    Add molecules  of customisation profile page

---
+++ web/src/components/molecules/prof-customs/Field.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface FieldProps {
+  label: string;
+  children: React.ReactNode;
+}
+
+export const Field = ({ label, children }: FieldProps) => {
+  return (
+    <div style={{ marginBottom: "14px" }}>
+      <label style={{ 
+        display: "block", 
+        fontSize: "12px", 
+        fontWeight: 500, 
+        color: "var(--color-text-secondary)", 
+        marginBottom: "5px" 
+      }}>
+        {label}
+      </label>
+      {children}
+    </div>
+  );
+};
